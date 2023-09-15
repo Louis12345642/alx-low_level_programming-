848ac3a98c325725b3c1486020e840c49fffdd4b
Mubarak Louis <mubaraklouis@gmail.com>
2023-09-15 13:00:12 -0700

    fixing number one

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,17 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * array_iterator - prints each array elem on a newl
- * @array: array
- * @size: how many elem to print
- * @action: pointer to print in regular or hex
- * Return: void
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
 {
-	unsigned int i;
-
-	if (array == NULL || action == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+	f(name);
 }
 
 
