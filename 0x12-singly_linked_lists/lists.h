ec063b8d5faf9ada7e3b0b16029c6a3712ac7326
Mubarak Louis <mubaraklouis@gmail.com>
2023-09-21 04:03:32 -0700

    fix the error

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-
-void free_list(list_t *head);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-size_t print_list(const list_t *h);
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
@@ -14,4 +17,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif
+
