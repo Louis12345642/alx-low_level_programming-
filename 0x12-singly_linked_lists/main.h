7cf59028e1547292f7ce31a6d448ceaba48128ae
Mubarak Louis <mubaraklouis@gmail.com>
2023-09-21 03:39:48 -0700

    fixing the error

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -6,4 +6,11 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 size_t print_list(const list_t *h);
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 #endif
