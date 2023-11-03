3b7365de83db91138ca39d3cbb6fe4af23c2023a
Mubarak kual louis <mubaraklouis@gmail.com>
2023-11-03 21:18:53 +0200

    fix the bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	a = open(filename, O_WRONLY | O_APPEND);
-	b = write(o, text_content, l);
+	b = write(a, text_content, l);
 
 	if (a == -1 || b == -1)
 		return (-1);
