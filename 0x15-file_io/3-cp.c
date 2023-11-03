bb67922bd4afdcf67482d15572b994deccba2a39
Mubarak kual louis <mubaraklouis@gmail.com>
2023-11-03 21:17:26 +0200

    [task:4] make the task number four

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - adds a text at the end of the file.
+ * @filename: file string file.
+ * @text_content: adds the string at the end of the file.
+ *
+ * Return: incase the function fails - -1.
+ *         incase the file exits it lack permission - -1.
+ *         Otherwise - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int a, b, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	a = open(filename, O_WRONLY | O_APPEND);
+	b = write(o, text_content, l);
+
+	if (a == -1 || b == -1)
+		return (-1);
+
+	close(a);
+
+	return (1);
+}
